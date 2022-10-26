d5b8073f1a1ccb05e5f64bda21a030acdb1e5d35
NusratJahanBristy <hellonusratjahanbristy@gmail.com>
2022-10-26 21:06:53 +0600

    cors added

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require("express")
 const app = express();
+const cors = require('cors');
 const Port = process.env.Port || 5000;
+app.use(cors());
+
+const categories = require('./data/categories.json');
 
 app.get('/', (req, res) => {
     res.send("digital edu server is running");
-  });
-  
-  app.listen(Port, () => {
+});
+
+app.get('/news-categories', (req, res) => {
+    res.send(categories)
+});
+
+app.listen(Port, () => {
     console.log("digital edu server is running", Port);
-  });
+});
