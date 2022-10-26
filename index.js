9d45d39b0c5afaf4d332415a14b4230148af78fd
NusratJahanBristy <hellonusratjahanbristy@gmail.com>
2022-10-26 19:03:10 +0600

    port running check

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-const express = require("express");
+const express = require("express")
 const app = express();
+const Port = process.env.Port || 5000;
+
+app.get('/', (req, res) => {
+    res.send("digital edu server is running");
+  });
+  
+  app.listen(Port, () => {
+    console.log("digital edu server is running", Port);
+  });
